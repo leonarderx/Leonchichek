8b71896f06c8fb0f5d0d4ba1c422ba0701da01f1
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-10-30 19:23:18 +0300

    Add files via upload

---
+++ Sourcedtfhjrtj.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+using namespace std;
+//1
+int main() {
+	/*int = 2;
+	do {
+		cout << i << " ";
+		i += 2;
+
+	} while (i <= 100);
+}*/
+
+//2
+	/*int a, b, i; sum = 0;
+	cin >> a >> b;
+	i = b
+		do {
+			if (i % 2 != 0) {
+				sum += i
+			}
+			i--;
+
+		} while (i >= a);
+		cout << sum;
+} */
+
+//3
+	/*int n; 
+	int i = 1;
+	cin >> n;
+	do {
+		cout << i * i << " ";
+		i++;
+
+
+	} while (i*i <= n);
+
+}*/
+
+
+//29
+	/*int i = 1;
+do {
+	cout << i << " ";
+	i += 2;
+
+} while (i <= 100);
+}*/
+
+
+	//30
+
+	//int sum = 0;
+	/*int i = 1;
+do {
+	cout << i << " ";
+	sum += i;
+	i += 2;
+
+} while (i <= 100);
+}*/
+
+	//17
+	
+	/*int n, rost;
+	cin >> n;
+	int i=1;
+	double sum = 0;
+do { 
+	cin >> rost;
+	sum += rost;
+	i++;
+
+
+} while (i <= n);
+
+cout << sum / n;
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
